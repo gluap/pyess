bd4f5328d1057b0e975be1711944026d4468b87f
paulg <github@pgoergen.de>
2019-10-03 15:34:53 +0200

    add command to fetch data from cli

--- pyess/cli.py
+++ pyess/cli.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import logging
+import json
+import urllib3
 import argparse
-from pyess.ess import find_all_esses, get_ess_pw, extract_name_from_zeroconf
+from pyess.ess import find_all_esses, get_ess_pw, extract_name_from_zeroconf, autodetect_ess, ESS
+from pyess.constants import STATE_URLS
 
 log = logging.getLogger(__name__)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def list_esses():
+
+def list_esses(args):
     print("Searching for LG ESS devices on the network")
     esses = find_all_esses()
     print("found {} devices:".format(len(esses)))
@@ -17,13 +22,25 @@ def list_esses():
         i += 1
 
 
-def get_password():
-    print("Fetching ess password. For this you must be connected to the ESS wifi (or recently have been connected to it)")
+def get_password(args):
+    print(
+        "Fetching ess password. For this you must be connected to the ESS wifi (or recently have been connected to it)")
     pw = get_ess_pw("192.168.1.253")
     return
 
 
-actions = {"list_ess": list_esses, "get_password": get_password}
+def get_data(args):
+    if not args.password:
+        print("--password must be given for get_data")
+    result = {}
+    ip, name = autodetect_ess()
+    ess = ESS(name, args.password)
+    for i in STATE_URLS.keys():
+        result[i] = ess.get_state(i)
+    print(json.dumps(result))
+
+
+actions = {"list_ess": list_esses, "get_password": get_password, "get_data": get_data}
 
 
 def main():
@@ -33,10 +50,12 @@ def main():
         choices=['debug', 'info', 'warning', 'error', 'critical'],
     )
     parser.add_argument("--action", default="list_ess", help="what to do", choices=actions.keys())
+    parser.add_argument("--password", default=None, help="password (required for get_data)")
+
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel.upper())
 
-    actions[args.action]()
+    actions[args.action](args)
 
 
 if __name__ == "__main__":
