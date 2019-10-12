468377fc13e1f0f5da5d37e667e83d439223a345
paulg <github@pgoergen.de>
2019-10-12 14:55:52 +0200

    adapt to api change

--- pyess/tests/cli_offline_test.py
+++ pyess/tests/cli_offline_test.py
@@ -14,7 +14,7 @@ def using_network():
 @pytest.mark.vcr(mode="all")
 def test_offline_log_against_graphite(password, mocker):
     mocker.patch('pyess.cli.autodetect_ess', return_value=["192.168.1.253", "THE_ESS_NAME"])
-    mocker.patch('pyess.ess.get_ess_ip', return_value=["192.168.1.253", "THE_ESS_NAME"])
+    mocker.patch('pyess.ess.get_ess_ip', return_value=["192.168.1.253"])
 
 
     pyess.cli.main(["--action", "log_against_graphite", "--graphite", "127.0.0.1", "--password", password, "--once", "true"])
@@ -23,7 +23,7 @@ def test_offline_log_against_graphite(password, mocker):
 @pytest.mark.vcr(mode="all")
 def test_offline_cli_get_data(password,mocker):
     mocker.patch('pyess.cli.autodetect_ess', return_value=["192.168.1.253", "THE_ESS_NAME"])
-    mocker.patch('pyess.ess.get_ess_ip', return_value=["192.168.1.253", "THE_ESS_NAME"])
+    mocker.patch('pyess.ess.get_ess_ip', return_value=["192.168.1.253"])
 
     pyess.cli.main(["--action", "get_data",  "--password", password])
 
