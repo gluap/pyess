4273a4d664876e82197943d78f9a371b4db8170a
paulg <github@pgoergen.de>
2020-04-13 15:46:35 +0200

    require python 3.7

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='pyess',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           "Operating System :: OS Independent",
